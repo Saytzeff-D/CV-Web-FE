f93036bb4036bb901492b43911ce22b377181e9b
David Atanda <ololadedavid15@gmail.com>
2025-10-04 16:10:36 +0100

    applied styles to NotFound page and Navbar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
       </BrowserRouter>
     </div>
