24b7f5c295d0cc768b12c7636ecdec0491521b66
David Atanda <ololadedavid15@gmail.com>
2025-10-05 01:13:50 +0100

    app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
+import Blog from './pages/Blog';
+import Buy from './pages/Buy';
+import Rent from './pages/Rent';
+import Shortlet from './pages/Shortlet';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/buy/:type' element={<Buy />} />        
+        <Route path='/rent/:type' element={<Rent />} />        
+        <Route path='/shortlet/:type' element={<Shortlet />} />        
         <Route path='/contact' element={<Contact />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
