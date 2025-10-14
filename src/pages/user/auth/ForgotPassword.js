c0627e6a5e7cfca2b9e46bd1ec9bd3cbed01fb7a
David Atanda <ololadedavid15@gmail.com>
2025-10-14 14:57:48 +0100

    feat: forgot password

---
+++ src/pages/user/auth/ForgotPassword.js
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const ForgotPassword = () => {  
+    const navigate = useNavigate()
+    const [checkEmail, setCheckEmail] = useState(false)
+
+    const verifyCode = () => {
+        navigate('/reset-password')
+        // Verification logic here
+    }
+
+    const checkUser = () => {
+        setCheckEmail(true)
+    }
+
+  return (
+    <>        
+        <div
+            className="d-flex justify-content-center align-items-center py-2"
+            style={{ minHeight: "100vh" }}
+        >
+            <div
+                className=""
+                style={{                            
+                padding: "40px 32px",
+                width: "100%",
+                maxWidth: "420px",
+                }}
+            >
+                {/* Header */}
+                <h4 className="text-center fw-semibold mb-1">Forgot Password</h4>
+                {
+                    !checkEmail
+                    ?
+                    <>
+                        <p
+                            className="text-center text-muted mb-4"
+                            style={{ fontSize: "14px" }}
+                        >
+                            No worries, we’ll send your reset instructions
+                        </p>
+                        <div className="mb-3">
+                            <label className="form-label fw-medium" style={{ fontSize: "13px" }}>
+                            Email Address
+                            </label>
+                            <input
+                            type="email"
+                            className="form-control"
+                            placeholder="falanayemi.com"
+                            style={{
+                                border: "1px solid #e9e9e9",
+                                height: "42px",
+                                fontSize: "14px",
+                            }}
+                            />
+                        </div>
+                        <button
+                            onClick={checkUser}
+                            type="submit"
+                            className="btn w-100 fw-semibold mb-4"
+                            style={{
+                            backgroundColor: "#004225",
+                            color: "#fff",
+                            borderRadius: "6px",
+                            height: "45px",
+                            }}
+                        >
+                            Continue
+                        </button>
+                    </>                
+                    :
+                    <>
+                        <p
+                            className="text-center text-muted mb-4"
+                            style={{ fontSize: "14px" }}
+                        >
+                            Check <b>Sogojames@gmail.com</b> to verify your account and get started
+                        </p>
+                            
+                        <div>               
+                            <div className="mb-3">
+                                <label className="form-label fw-medium" style={{ fontSize: "13px" }}>
+                                Enter Code
+                                </label>
+                                <input
+                                type="number"
+                                className="form-control"
+                                placeholder="123456"
+                                style={{
+                                    border: "1px solid #e9e9e9",
+                                    height: "42px",
+                                    fontSize: "14px",
+                                }}
+                                />
+                            </div>                            
+
+                            <button
+                                onClick={verifyCode}
+                                type="submit"
+                                className="btn w-100 fw-semibold"
+                                style={{
+                                backgroundColor: "#004225",
+                                color: "#fff",
+                                borderRadius: "6px",
+                                height: "45px",
+                                }}
+                            >
+                                Continue
+                            </button>
+
+                            <p className="text-center mt-3 mb-0" style={{ fontSize: "13px" }}> 
+                                Didn't receive the mail? {" "}
+                                <a className="fw-semibold">
+                                    Click Me
+                                </a>
+                            </p>
+                        </div>
+                    </>
+                }
+                <p className="text-center">
+                    <Link to={'/login'} className="fw-semibold" style={{ fontSize: "13px" }}>
+                        <i className="fa fa-arrow-left"></i> Back to Login
+                    </Link>
+                </p>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default ForgotPassword;
