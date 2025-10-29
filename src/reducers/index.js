e6a736494bd1249b0e48d4df3e84926868bde8b4
David Atanda <ololadedavid15@gmail.com>
2025-10-29 21:09:00 +0100

    feat: reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const initState = {
+    uri: 'https://cv-web-be.onrender.com/api/'
+    // uri: 'http://api.cvproperties.co/api/'
+}
+
+const Reducer = (state=initState, action)=>{
+    return state
+}
+
+export default Reducer
